10f752643ca23447a00c9a84fc07743db567a3ee
albert <xins@bupt.edu.cn>
2022-06-16 17:02:02 +0800

    use animation to draw real-time traffic and solve matplotlib chinese support problem

--- simulations/DDosDetection.py
+++ simulations/DDosDetection.py
@@ -1,154 +1,204 @@
 import pandas as pd
 import numpy as np
+import matplotlib as mpl
 import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+from matplotlib.lines import Line2D
 import sys
 import signal
 
-def parse_ndarray(s):
-    return np.fromstring(s, sep=' ') if s else None
 
-def Mu(nowTraffic, nowTrafficMean, l, h):
-    if nowTraffic < (l * nowTrafficMean):
-        return 0
-    elif nowTraffic > (h * nowTrafficMean):
-        return 1
-    else:
-        first = nowTraffic / nowTrafficMean - l
-        second = h - l
-        return first / second
+class Hyperparameter:
 
-def TrafficMean(t, lastTrafficMean, nowTraffic):
-    return ((t - 1) * lastTrafficMean + nowTraffic) / t
+    def __init__(self):
+        self.load_limit = 0
+        self.test_cycle = 0
+        self.threshold_low = 0
+        self.threshold_high = 0
 
-def Dvar(t, lastDvar, nowTraffic, lastTraffic):
-    if t < 2:
-        return 0
-    z = nowTraffic - lastTraffic
-    first = (t - 2) * lastDvar + np.square(z)
-    second = t - 1
-    return first / second
+class Detecter:
 
-def GetAlertTime(traffic, peakTime, peakTraffic, testCycle, l, h):
-    alertTime = []
-    a = 0
-    nowA = 0
-    lastA = 0
-    nowMu = 0
-    lastMu = 0
-    isComputerTrafficMean = True
-    trafficMean = np.zeros(peakTime)
-    dvar = np.zeros(peakTime)
-    for t in np.arange(1, peakTime):
-        if traffic[t] == 0:
-            s = sys.maxsize
-        else:
-            s = (testCycle * peakTraffic / traffic[t]).astype(int)
-        dvar[t] = Dvar(t, dvar[t - 1], traffic[t], traffic[t - 1])
-        trafficMean[t] = trafficMean[t - 1]
-        if s == 0:
-            alertTime.append(t)
-            isComputerTrafficMean = False
-            nowA = 1
-            nowMu = 1
-        else:
-            if isComputerTrafficMean:
-                trafficMean[t] = TrafficMean(t, trafficMean[t - 1], traffic[t])
-            nowMu = Mu(traffic[t], trafficMean[t], l, h)
-            if nowMu > 0:
-                testCycle += 1
-                a += 1
-                nowA = max(nowMu, lastMu) if dvar[t] < dvar[t - 1] else nowMu
-                if a >= s:
-                    if nowA > lastA:
-                        alertTime.append(t)
-                        isComputerTrafficMean = False
-                    else:
-                        a = s - 1
+    def __init__(self, ax, file_path, statistic_name,  hyperparameter):
+        self.ax = ax
+
+        self.file_path = file_path
+        self.statistic_name = statistic_name
+
+        self.load_limit = hyperparameter.load_limit
+        self.test_cycle = hyperparameter.test_cycle
+        self.threshold_low = hyperparameter.threshold_low
+        self.threshold_high = hyperparameter.threshold_high
+
+        self.time_seq = None
+        self.recived_packets = None
+        self.traffic = [0]
+        self.traffic_mean = [0]
+        self.dvar = [0]
+        self.time = [0]
+        self.max_time = 0
+
+        self.line_traffic_mean = Line2D(self.time, self.traffic_mean, label='流量均值')
+        self.line_normal_traffic = Line2D(self.time, self.traffic, color='g', label='正常流量')
+        self.line_alert_traffic = Line2D(self.time, self.traffic, color='r', label='异常流量')
+
+        self.ax.add_line(self.line_traffic_mean)
+        self.ax.add_line(self.line_normal_traffic)
+        self.ax.add_line(self.line_alert_traffic)
+        ax.legend()
+    
+    def GetVector(self):
+        try:
+            statistics = pd.read_csv(self.file_path, converters={
+                'vectime': parse_ndarray,
+                'vecvalue': parse_ndarray
+            })
+            target_row = statistics[(statistics.type == 'vector') & (statistics.name == self.statistic_name)].iloc[0]
+            self.time_seq = target_row.vectime
+            self.recived_packets = target_row.vecvalue
+        except:
+            self.time_seq = None
+            self.recived_packets = None
+
+    def GetTraffic(self):
+        self.max_time = max(self.time_seq) + 1
+        indexPacketNum = np.zeros(self.max_time, dtype=int)
+        for i in range(0, self.max_time):
+            indexArr = np.argwhere(self.time_seq == i)
+            if len(indexArr) > 0:
+                indexPacketNum[i] = indexArr[-1]
             else:
-                trafficMean[t] = TrafficMean(t, trafficMean[t - 1], traffic[t])
-                isComputerTrafficMean = True
-                nowA = 0
-                a = 0
-        lastA = nowA
-        lastMu = nowMu
+                indexPacketNum[i] = indexPacketNum[i - 1]
+        self.traffic = np.zeros(self.max_time, dtype=float)
+        for t in range(0, self.max_time):
+            self.traffic[t] = self.recived_packets[indexPacketNum[t]]
+        self.traffic = np.diff(self.traffic)
+        self.traffic = np.insert(self.traffic, 0, 0)
 
-    return alertTime, trafficMean
-
-def GetVector(filePath, statisticName):
-    try:
-        statistics = pd.read_csv(filePath, converters={
-            'vectime': parse_ndarray,
-            'vecvalue': parse_ndarray
-        })
-        targetRow = statistics[(statistics.type == 'vector') & (statistics.name == statisticName)].iloc[0]
-    except:
-        return None, None
-
-    timeSeq = targetRow.vectime
-    recivedPackets = targetRow.vecvalue
-    return timeSeq, recivedPackets
-
-def GetTraffic(timeSeq, recivedPackets):
-    peakTime = max(timeSeq) + 1
-    indexPacketNum = np.zeros(peakTime, dtype=int)
-    for i in range(0, peakTime):
-        indexArr = np.argwhere(timeSeq == i)
-        if len(indexArr) > 0:
-            indexPacketNum[i] = indexArr[-1]
+    def Mu(self, t):
+        if self.traffic[t] < (self.threshold_low * self.traffic_mean[t]):
+            return 0
+        elif self.traffic[t] > (self.threshold_high * self.traffic_mean[t]):
+            return 1
         else:
-            indexPacketNum[i] = indexPacketNum[i - 1]
-    traffic = np.zeros(peakTime, dtype=float)
-    for t in range(0, peakTime):
-        traffic[t] = recivedPackets[indexPacketNum[t]]
-    traffic = np.diff(traffic)
-    traffic = np.insert(traffic, 0, 0)
+            first = self.traffic[t] / self.traffic_mean[t] - self.threshold_low
+            second = self.threshold_high - self.threshold_low
+            return first / second
 
-    return traffic
+    def Dvar(self, t):
+        if t < 2:
+            return 0
+        z = self.traffic[t] - self.traffic[t-1]
+        first = (t - 2) * self.dvar[t - 1] + np.square(z)
+        second = t - 1
+        self.dvar[t] = first / second
 
-def Draw(filePath, statisticName, peakTraffic, i, l, h):
-    plt.ion()
-    while isDone == False:
-        timeSeq, recivedPackets = GetVector(filePath, statisticName)
-        if (not timeSeq is None) and (not recivedPackets is None):
-            plt.clf()
-            timeSeq = timeSeq.astype(int)
-            recivedPackets = recivedPackets.astype(int)
-            traffic = GetTraffic(timeSeq, recivedPackets)
-            peakTime = max(timeSeq) + 1
-            alertTimeList, trafficMean = GetAlertTime(traffic, peakTime, peakTraffic, i, l, h)
-            time = np.arange(0, peakTime)
-            alertTime = np.ma.masked_where(np.isin(time, alertTimeList, invert=True), time)
-            normalTime = np.ma.masked_where(np.isin(time, alertTimeList, invert=False), time)
+    def GetAlertTime(self):
+        alert_time = []
+        self.traffic_mean = np.zeros(self.max_time)
+        self.dvar = np.zeros(self.max_time)
 
-            agraphic = plt.subplot()
-            agraphic.set_title('subplot')  # 添加子标题
-            agraphic.set_xlabel('x axis', fontsize=10)  # 添加轴标签
-            agraphic.set_ylabel('y axis', fontsize=20)
-            agraphic.plot(time, trafficMean, label='TrafficMean')  # 等于agraghic.plot(ax,ay,'g-')
-            agraphic.plot(normalTime, traffic, color='g', label='NormalTraffic')
-            agraphic.plot(alertTime, traffic, color='r', label='Alert')
-            # agraphic.plot(ndarrTime, ndarrDvar, label='Dvar') 差分方差
-            agraphic.legend()
-            plt.pause(0.3)
-    plt.ioff()
-    plt.show()
+        a = 0
+        A = 0
+        last_A = 0
+        mu = 0
+        last_mu = 0
+        is_compute_trafficmean = True
+        for t in np.arange(1, self.max_time):
+            if self.traffic[t] == 0:
+                s = sys.maxsize
+            else:
+                s = int(self.test_cycle * self.load_limit / self.traffic[t])
+            self.Dvar(t)
+            self.traffic_mean[t] = self.traffic_mean[t - 1]
+            if s == 0:
+                alert_time.append(t)
+                is_compute_trafficmean = False
+                A = 1
+                mu = 1
+            else:
+                if is_compute_trafficmean:
+                    self.traffic_mean[t] = ((t - 1) * self.traffic_mean[t - 1] + self.traffic[t]) / t
+                mu = self.Mu(t)
+                if mu > 0:
+                    self.test_cycle += 1
+                    a += 1
+                    A = max(mu, last_mu) if self.dvar[t] < self.dvar[t - 1] else mu
+                    if a >= s:
+                        if A > last_A:
+                            alert_time.append(t)
+                            is_compute_trafficmean = False
+                        else:
+                            a = s - 1
+                else:
+                    self.traffic_mean[t] = ((t - 1) * self.traffic_mean[t - 1] + self.traffic[t]) / t
+                    is_compute_trafficmean = True
+                    A = 0
+                    a = 0
+            last_A = A
+            last_mu = mu
+
+        return alert_time
+
+    def Update(self, i):
+        # 获取最新数据
+        self.GetVector()
+        if (not self.time_seq is None) and (not self.recived_packets is None):
+            self.time_seq = self.time_seq.astype(int)
+            self.recived_packets = self.recived_packets.astype(int)
+            self.GetTraffic()
+            alert_time_list = self.GetAlertTime()
+            self.time = np.arange(0, self.max_time)
+            self.alert_time = np.ma.masked_where(np.isin(self.time, alert_time_list, invert=True), self.time)
+            self.normal_time = np.ma.masked_where(np.isin(self.time, alert_time_list, invert=False), self.time)
+
+            # 更新坐标轴
+            xmin, xmax = ax.get_xlim()
+            if self.max_time > xmax:
+                ax.set_xlim(0, 2 * self.max_time)
+                ax.figure.canvas.draw()
+            ymin, ymax = ax.get_ylim()
+
+            max_statistic_value = np.max(np.append(self.traffic, self.traffic_mean))
+            if max_statistic_value > ymax:
+                ax.set_ylim(0, 2 * max_statistic_value)
+                ax.figure.canvas.draw()
+
+            # 更新折线
+            self.line_traffic_mean.set_data(self.time, self.traffic_mean)
+            self.line_normal_traffic.set_data(self.normal_time, self.traffic)
+            self.line_alert_traffic.set_data(self.alert_time, self.traffic)
+            # self.line_traffic_dvar(time, ndarrDvar, label='Dvar', label='差分方差')
+
+        return self.line_traffic_mean, self.line_normal_traffic, self.line_alert_traffic,
+
+def parse_ndarray(value):
+    return np.fromstring(value, sep=' ') if value else None
 
 def SignalHandler(signum, frame):
-    global isDone
-    isDone = True
+    ani.pause()
 
-
-isDone = False
 
 signal.signal(signal.SIGTERM, SignalHandler)
-filePath = './results/traffic.csv'
-statisticName = 'NumReceivedIpPackets:vector'
 
-peakTraffic = 30
-i = 2
-l = 2.5
-h = 3.5
+file_path = './results/traffic.csv'
+statistic_name = 'NumReceivedIpPackets:vector'
 
-Draw(filePath, statisticName, peakTraffic, i, l, h)
+hyperparameter = Hyperparameter()
+hyperparameter.load_limit = 30
+hyperparameter.test_cycle = 2
+hyperparameter.threshold_low = 2.5
+hyperparameter.threshold_high = 3.5
 
+mpl.rcParams[u'font.sans-serif'] = ['simhei']
 
+fig, ax = plt.subplots()
+ax.set_title('实时流量统计')  # 添加子标题
+ax.set_xlabel('时间', fontsize=10)  # 添加轴标签
+ax.set_ylabel('流量', fontsize=20)
+ax.grid()
+
+detecter = Detecter(ax, file_path, statistic_name, hyperparameter)
+
+ani = animation.FuncAnimation(fig, detecter.Update, interval=500)
+
+plt.show()
